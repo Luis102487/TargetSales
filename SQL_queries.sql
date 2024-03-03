3dc7f7db14252ba3a009aab7fac20a08a0480cd8
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-03-03 15:00:11 -0500

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,2 +1,20 @@
-SELECT *
-FROM target_sales;
+----Create Table 
+CREATE OR REPLACE TABLE luisalva.walmart_dataset.sales(
+	  invoice_id STRING PRIMARY KEY,
+    branch STRING NOT NULL,
+    city STRING NOT NULL,
+    customer_type STRING NOT NULL,
+    gender STRING NOT NULL,
+    product_line STRING NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    tax_pct DECIMAL NOT NULL,
+    total DECIMAL(12, 4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment STRING NOT NULL,
+    cogs DECIMAL(10,2) NOT NULL,
+    gross_margin_pct DECIMAL (11,9),
+    gross_income DECIMAL(12, 4),
+    rating DECIMAL(2, 1)
+);
